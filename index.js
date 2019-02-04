712598530bc0b611445200ffa15867c212d326d8
oleg79 <0112oleg@gmail.com>
2019-02-04 03:27:49 +0300

    ADDED: a simple API

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require('http')
+const url = require('url')
+
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url)
+    const path = parsedUrl.path.replace(/^\/+|\/+$/g, '')
+
+    req.on('data', () => {})
+
+    req.on('end', () => {
+        res.setHeader('Content-Type', 'application/json')
+        if (path === 'hello') {
+            res.writeHead(200)
+            res.end(JSON.stringify({ message: 'Hello World!' }))
+        } else {
+            res.writeHead(404)
+            res.end(JSON.stringify({ error: 'not found' }))
+        }
+    })
+})
+
+const port = 3000
+server.listen(port, () => {
+    console.log(`The server is running on port: ${port}`)
+})
